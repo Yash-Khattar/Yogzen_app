8562f0af07b172b1382710552cafe38717337ecf
Yash-Khattar <yashkhattar73@gmail.com>
2023-11-12 20:18:35 +0530

    welcome screen animation

---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:yogzen/global/color.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with TickerProviderStateMixin {
+  AnimationController? controller;
+  Animation<double>? animation;
+  bool reverse = true;
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      vsync: this,
+    );
+    animation = Tween<double>(
+      begin: -50,
+      end: 50,
+    ).animate(controller!)
+      ..addListener(() {
+        setState(() {});
+        if (controller!.isCompleted) {
+          controller!.reverse();
+        }
+        if (controller!.isDismissed) {
+          controller!.forward();
+        }
+      });
+
+    controller!.forward();
+    // The looping is done via listener.
+  }
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: klightBlue,
+        body: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Spacer(),
+              Text(
+                'Yogzen',
+                style: TextStyle(
+                    color: kblackHeading,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w600),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Text(
+                'AI enabled yoga app for all your ',
+                style: TextStyle(color: kblackSubHeading),
+              ),
+              Text(
+                'yogic needs!',
+                style: TextStyle(color: kblackSubHeading),
+              ),
+              Spacer(),
+              AnimatedContainer(
+                  duration: Duration(milliseconds: 1500),
+                  transform: Transform.translate(
+                    offset: Offset(0, animation!.value),
+                  ).transform,
+                  child: Image.asset('assets/welcome.png')),
+              Spacer(),
+              ElevatedButton(
+                onPressed: () {
+                  // Navigator.popAndPushNamed(context, "/tabScreen");
+                },
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: kdarkBlue,
+                    foregroundColor: Colors.white,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(100))),
+                child: const Padding(
+                  padding: EdgeInsets.fromLTRB(50, 14, 50, 14),
+                  child: Text('Get Started', style: TextStyle(fontSize: 16)),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+            ],
+          )
+        ]));
+  }
+}
